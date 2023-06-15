f6c746bd16a8367ae399e9228e16a74795ea5e95
Walin Paredes <walinparedes3010@gmail.com>
2023-06-15 10:49:58 -0500

    U Errores de referencia (img) E | BJ

--- assets/js/alerts.js
+++ assets/js/alerts.js
@@ -5,18 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
     let timerInterval;
     Swal.fire({
         title: 'Bienvenido, vamos a jugar BlackJack!',
-        imageUrl: '/assets/img/fondo-game.png',
+        imageUrl: 'assets/img/fondo-game.png',
         imageWidth: 400,
         imageHeight: 200,
         html: ' Comenzará en <b></b> segundos.',
-        timer: 6000,
+        timer: 5000,
         timerProgressBar: true,
         didOpen: () => {
             Swal.showLoading()
             const b = Swal.getHtmlContainer().querySelector('b')
             timerInterval = setInterval(() => {
                 b.textContent = Math.round(Swal.getTimerLeft() / 1000);
-            }, 100)
+            }, 110)
         },
         willClose: () => {
             clearInterval(timerInterval)
@@ -29,26 +29,26 @@ document.addEventListener('DOMContentLoaded', () => {
         Swal.fire({
             title: 'Pulsa el botón <b>Nuevo Juego</b> para comenzar',
             text: 'Luego de pulsarlo se habilitarán Pedir Carta y Detener Juego',
-            imageUrl: '/assets/img/fondo-sw-br.png',
+            imageUrl: 'assets/img/fondo-sw-br.png',
             imageWidth: 400,
             imageHeight: 200,
-            imageAlt: 'Custom image',
+            imageAlt: 'NuevoJuego',
         }).then(() => {
             Swal.fire({
                 title: 'Pulsa el botón <b>Pedir Cartas</b>',
                 text: 'Te daremos cartas al azar, tendrás que llegar a 21 o tener un puntaje muy cercano',
-                imageUrl: '/assets/img/fondo-sw-pc.jpeg',
+                imageUrl: 'assets/img/fondo-sw-pc.jpeg',
                 imageWidth: 400,
                 imageHeight: 200,
-                imageAlt: 'Custom image',
+                imageAlt: 'PedirCartas',
             }).then(() => {
                 Swal.fire({
                     title: 'Pulsa el botón <b>Detener Juego</b>',
                     text: 'Pulsalo solo cuando crees que vas a ganar, pero ten cuidado... quizá la pc te gane :)',
-                    imageUrl: '/assets/img/fondo-sw-tc.jpeg',
+                    imageUrl: 'assets/img/fondo-sw-tc.jpeg',
                     imageWidth: 400,
                     imageHeight: 200,
-                    imageAlt: 'Custom image',
+                    imageAlt: 'DetenerJuego',
                 });
             });
         });
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const ganaste = () => Swal.fire({
     title: 'Felicidades!',
     text: 'Has logrado ganarle a la computadora.',
-    imageUrl: '/assets/img/fondo-sw-w.jpg',
+    imageUrl: 'assets/img/fondo-sw-w.jpg',
     imageWidth: 330,
     imageHeight: 200,
     imageAlt: 'proplayer',
@@ -72,7 +72,7 @@ const ganaste = () => Swal.fire({
 const empate = () => Swal.fire({
     title: 'Oops..',
     text: 'Qué casualidad, tú IQ es igual al de la PC',
-    imageUrl: '/assets/img/fondo-sw-c.jpg',
+    imageUrl: 'assets/img/fondo-sw-c.jpg',
     imageWidth: 330,
     imageHeight: 200,
     imageAlt: 'empate-imagen',
@@ -82,7 +82,7 @@ const empate = () => Swal.fire({
 const perdiste = () => Swal.fire({
     title: 'Lo siento...',
     text: 'No has logrado ganarle a la pc, perdiste',
-    imageUrl: '/assets/img/fondo-sw-l.jpg',
+    imageUrl: 'assets/img/fondo-sw-l.jpg',
     imageWidth: 330,
     imageHeight: 200,
     imageAlt: 'loser',
